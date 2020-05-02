2995e140002fdc37486b982dedee08622ceccd77
Mansi2999 <bhagatmansi2999@gmail.com>
2020-05-02 22:36:40 +0530

    Publishing my 1st website

---
+++ home.php
@@ -0,0 +1,290 @@
+<html>
+<head>
+<title>Hostel Management</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+	<section id="nav-bar">
+		<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img src="image/cvmu.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#slider">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#facilities">FACILITIES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#fee">FEE PLANS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#application">ENROLL</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</section>
+
+
+<div id="slider">
+  <div id="headerslider" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#headerslider" data-slide-to="0" class="active"></li>
+    <li data-target="#headerslider" data-slide-to="1"></li>
+    <li data-target="#headerslider" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/MBICTBuilding.jpg" class="d-block img-fluid">
+      <div class="carousel-caption">
+        <h5>Welcome to CVM Campus!</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/h3.jpg" class="d-block img-fluid">
+      <div class="carousel-caption">
+        <h5>Welcome to Sharda Hostel!</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/ha3.jpg" class="d-block img-fluid">
+      <div class="carousel-caption">
+        <h5>Home away from Home!</h5>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#headerslider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#headerslider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+	
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h2>ABOUT US</h2>
+          <div class="about content">
+            MBIT has a separate hostel section under the management of Hostel Management Committee. The Rector who is the secretary of the Committee administers the day to day aspects of the hostel management at MBIT Hostel. There are three hostels under MBIT College. The Sharada hostel provides the students with a safe, academic environment for the students of MBIT. The hostel has a Superintendent from the Non-teaching staff of the colleges who is responsible for the discipline, maintenance of the building, etc. The hostel also has two Rectors from the MBIT faculties. There is a mess associated with  Hostel for girls.  Membership of the mess is compulsory for students staying in MBIT College.
+          </div>
+          <button type="button" class="btn btn-primary">Read more</button>
+        </div>
+          <div class="col-md-6 skills-bar">
+            <p>MBIT</p>
+            <div class="progress">
+              <div class="progress-bar" style="width: 60%;">60%</div>
+            </div>
+            <p>ADIT</p>
+            <div class="progress">
+              <div class="progress-bar" style="width: 20%;">20%</div>
+            </div>
+            <p>CZ Patel</p>
+            <div class="progress">
+              <div class="progress-bar" style="width: 10%;">10%</div>
+            </div>
+            <p>ARIBAZ</p>
+            <div class="progress">
+              <div class="progress-bar" style="width: 10%;">10%</div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <section id="facilities">
+    <div class="container">
+      <h1>FACILITIES</h1>
+      <div class="row facilities">
+        <div class="col-md-4 text-center">
+          <div class="icon">
+            <i class="fa fa-snowflake-o"></i>
+          </div>
+          <h3>Air Conditioned Rooms</h3>
+          <p>Sharda Hostel also provides the faclity of Air Conditioned Rooms to their students on minimal difference in the fee.</p>
+          </div>
+          <div class="col-md-4 text-center">
+          <div class="icon">
+            <i class="fa fa-wifi"></i>
+          </div>
+          <h3>WIFI Connection</h3>
+          <p>The students need to do project and research work with the help of internet that is why sharda hostel provides them with a quality connection of WIFI.</p>
+          </div>
+          <div class="col-md-4 text-center">
+          <div class="icon">
+            <i class="fa fa-cutlery"></i>
+          </div>
+          <h3>Quality Food</h3>
+          <p>We don't want our students to feel homesick, and hence we take care of their taste!.</p>
+          </div>
+          <div class="col-md-4 text-center">
+          <div class="icon">
+            <i class="fa fa-trash-o" aria-hidden="true"></i>
+          </div>
+          <h3>Daily cleaning service</h3>
+          <p>Sharda Hostel also provides the faclity of daily room cleaning services to maintain the hygine.</p>
+          </div>
+          <div class="col-md-4 text-center">
+          <div class="icon">
+           <i class="fa fa-shield"></i> 
+          </div>
+          <h3>Security</h3>
+          <p>We believe in protecting our students and thus sharda hostel is protected with professional security.</p>
+          </div>
+          <div class="col-md-4 text-center">
+          <div class="icon">
+            <i class="fa fa-building-o"></i>
+          </div>
+          <h3>Alivator</h3>
+          <p>Sharda Hostel has multiple floors and blocks, looking after their comfort our hostel has a facility of an alivator</p>
+          </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="fee">
+    <div class="container">
+      <h1>FEE PLANS</h1>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="single-price">
+            <div class="price-head">
+              <h2>Regular Room</h2>
+              <p>22,000 INR/<span>year</span></p>
+               </div>
+               <div class="price-content">
+                 <ul>
+                   <li>Wifi service</li>
+                   <li>Attached Washroom</li>
+                   <li>Cleaning service</li>
+                   <li>Study table</li>
+                 </ul>
+               </div>
+               <div class="price-button">
+                 <a class="enroll-btn" href="#">Enroll Now</a>
+               </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="single-price">
+            <div class="price-head">
+              <h2>Deluxe Room</h2>
+              <p>30,000 INR/<span>year</span></p>
+               </div>
+               <div class="price-content">
+                 <ul>
+                   <li>Air Conditioner</li>
+                   <li>Window net</li>
+                   <li>Renovated room</li>
+                   <li>Wifi service</li>
+                   <li>Attached Washroom</li>
+                   <li>Cleaning service</li>
+                   <li>Study table</li>
+                 </ul>
+               </div>
+               <div class="price-button">
+                 <a class="enroll-btn" href="#">Enroll Now</a>
+               </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="single-price">
+            <div class="price-head">
+              <h2>Guest Room</h2>
+              <p>250 INR/<span>day</span></p>
+               </div>
+               <div class="price-content">
+                 <ul>
+                  <li>Air Conditioner</li>
+                   <li>Window net</li>
+                   <li>Renovated room</li>
+                   <li>Wifi service</li>
+                   <li>Attached Washroom</li>
+                   <li>Cleaning service</li>
+                   <li>Study table</li>
+                   </ul>
+               </div>
+               <div class="price-button">
+                 <a class="enroll-btn" href="#">Enroll Now</a>
+               </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<section id="application">
+  <div class="container">
+    <h1>Enroll Now!</h1>
+    <div class="shardahostel">
+      <h2>Sharda Hostel</h2>
+    </div>
+    <div class="appform">
+      <form>
+        <div id="name">
+          <h3 class="name">Name</h3>
+          <input class="firstname" type="text" name="first_name"><br>
+          <label class="firstlabel">first name</label>
+          <input class="lastname" type="text" name="last_name"><br>
+          <label class="lastlabel">last name</label>
+          </div>
+          <h3 class="name">Email Id</h3>
+          <input class="email" type="text" name="email">
+          <h3 class="name">Contact</h3>
+          <input class="number" type="text" name="contact">
+          <h3 class="name">Address</h3>
+          <input class="address" type="text" name="address">
+          <h3 class="name">College</h3>
+          <input class="college" type="text" name="college">
+          <h3 class="name">Field</h3>
+          <input class="field" type="text" name="field">
+          <h3 class="name">Room type</h3>
+          <select class="option" name="room">
+            <option disabled="disabled" selected="selected">Choose</option>
+            <option>Regular room</option>
+            <option>Deluxe room</option>
+            <option>Guest room</option>
+          </select>
+          <button type="button" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</section>
+
+
+<footer>
+    <p class="p-3 bg-dark text-white text-center">&copy Developed by Mansi Bhagat</p>
+  </footer>
+
+  <script src="js/smooth-scroll.js"></script>
+  <script>
+  var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+
+</body>
+</html>
